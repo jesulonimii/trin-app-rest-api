06114ffaed68fa30269db4f9eff4bb730d576ce7
jesulonimii <jesulonimii.will@gmail.com>
2021-03-10 12:27:59 +0100

    initial commit

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
 
-//$db_host = "sql4.freemysqlhosting.net";
-//$db_dbname = "sql4397947";
-//$db_username = "sql4397947";
-//$db_password = "g8xNQ4uIxN";
+$db_host = "sql4.freemysqlhosting.net";
+$db_dbname = "sql4397947";
+$db_username = "sql4397947";
+$db_password = "g8xNQ4uIxN";
 
 
-$db_host = "localhost";
-$db_dbname = "epiz_27422941_trin_posts";
-$db_username = "root";
-$db_password = "";
+//$db_host = "localhost";
+//$db_dbname = "epiz_27422941_trin_posts";
+//$db_username = "root";
+//$db_password = "";
 
 
 $connection = mysqli_connect($db_host, $db_username, $db_password, $db_dbname);

1d21d924896ef3d1b66bb1c8ba5abd59e864a1c5
jesulonimii <jesulonimii.will@gmail.com>
2021-03-10 10:58:16 +0100

    set db

--- db.php
+++ db.php
@@ -1,9 +1,15 @@
 <?php
 
-$db_host = "localhost";
+$db_host = "sql104.epizy.com";
 $db_dbname = "epiz_27422941_trin_posts";
-$db_username = "root";
-$db_password = "";
+$db_username = "epiz_27422941";
+$db_password = "will4christ2";
+
+
+//$db_host = "localhost";
+//$db_dbname = "epiz_27422941_trin_posts";
+//$db_username = "root";
+//$db_password = "";
 
 
 $connection = mysqli_connect($db_host, $db_username, $db_password, $db_dbname);

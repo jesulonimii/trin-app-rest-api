bc5b6fa43ff23c6740ec96bf2fdad82572cf8103
William Abodunrin <jesulonimii.will@gmail.com>
2021-10-06 09:21:47 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,19 +2,21 @@
 
 //TODO: Move this somwhere else
 
+/*
 $db_host = "db4free.net";
 $db_dbname = "jesulonimii_trin";
 $db_username = "jesulonimii_trin";
 $db_password = "jesus4eva";
 
-/*
+*/
+
 
 $db_host = "sql4.freesqldatabase.com";
 $db_dbname = "sql4442496";
 $db_username = "sql4442496";
 $db_password = "5X3rueRCrc";
 
-*/
+
 
 $connection = mysqli_connect($db_host, $db_username, $db_password, $db_dbname);
 
